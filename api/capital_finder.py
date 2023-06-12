9866cf258caecc4e525570c0ab929e528a303c23
firas1awadallah <firasawa@gmail.com>
2023-06-12 17:29:33 +0000

    test5

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -22,7 +22,7 @@ class handler(BaseHTTPRequestHandler):
       data = res.json()
     #   print(222,data)
     for country_data in data :
-      Country = country_data['name']
+      Country = country_data['name']['common']
       message = str(Country)
     #   list_of_dif.append(message)
     # print(2222,list_of_dif)
