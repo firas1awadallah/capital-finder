68f9c4aad30c64166080817827e14a8c9dcfe172
firas1awadallah <firasawa@gmail.com>
2023-06-12 13:45:59 +0000

    firas

--- api/date.py
+++ api/date.py
@@ -1,5 +1,5 @@
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+
  
 class handler(BaseHTTPRequestHandler):
  
@@ -7,5 +7,6 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    message = "my name firas"
+    self.wfile.write(message.encode())
     return
