785872bf7470407f450795f4736ef6667fc264d7
firas1awadallah <firasawa@gmail.com>
2023-06-12 19:05:55 +0000

    test19

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -22,7 +22,7 @@ class handler(BaseHTTPRequestHandler):
       data = res.json()
     #   print(222,data)
     for word_data in data :
-      definition = word_data['capital']
+      definition = word_data['capital'][0]
       message = f"The capital of {word} is {definition}"
     #   list_of_dif.append(message)
     # print(2222,list_of_dif)
